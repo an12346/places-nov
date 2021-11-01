84fd108f1b4dbee2aab973e5ee6b0b1d60a4910c
Ella Tanttu <ellajtanttu@gmail.com>
2021-11-01 16:31:12 -0700

    add beginning ui functionality
    co-authored by: Anna Pittman <annaepittman@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business Logic:
 function Location(name, landmarks, time, notes) {
   this.name = name;
   this.landmarks = landmarks;
@@ -20,12 +21,134 @@ LocationBook.prototype.assignId = function() {
   return this.currentId;
 };
 
-// Constructors:
-//   * Each of us
-//     1. Ella
-//     2. Anna
-//   * Locations
-//   * Keys in Locations:
-//     - Landmarks - array
-//     - time of year - string
-//     - notes - string
+LocationBook.prototype.findLocation = function(id) {
+  if (this.locations[id] !== undefined) {
+    return this.location[id];
+  }
+  return false;
+}
+
+//User Interface Logic
+
+// $(document).ready(function() {
+//   $("#sanDiego").click(function() {
+//     $("#diegoResults").show();
+//   });
+//   $("#sedona").click(function() {
+//     $("#sedonaResults").show();
+//   });
+//   $("#francisco").click(function() {
+//     $("#franciscoResults").show();
+//   });
+//   $("#joshua").click(function() {
+//     $("#joshuaResults").show();
+//   });
+// });
+
+let LocationBook1 = new LocationBook();
+
+let location1 = new Location(
+  "X",
+  "Sedona",
+  ["Church of holy cross", "antelope canyon"],
+  "spring",
+  "sunsets are beautiful!" )
+
+let location2 = new Location(
+  "X",
+  "Sedona",
+  ["Church of holy cross", "antelope canyon"],
+  "spring",
+  "sunsets are beautiful!" )
+
+let location3 = new Location(
+  "X",
+  "Sedona",
+  ["Church of holy cross", "antelope canyon"],
+  "spring",
+  "sunsets are beautiful!" )
+
+let location4 = new Location(
+  "X",
+  "Sedona",
+  ["Church of holy cross", "antelope canyon"],
+  "spring",
+  "sunsets are beautiful!" )
+
+LocationBook1.addLocation(location1);
+LocationBook1.addLocation(location2);
+LocationBook1.addLocation(location3);
+LocationBook1.addLocation(location4);
+
+const name = "";
+
+
+$(document).ready(function() {
+  $("form#citySelector").submit(function(event) {
+    event.preventDefault();
+    $(".resultCard").hide();
+    const userSelection = $("input:radio[name=flexRadioDefault]:checked").val().toString();
+    if (userSelection === "sanDiego") {
+      $("#diegoResults").show();
+    } else if (userSelection === "sedona") {
+      $("#sedonaResults").show();
+    } else if (userSelection === "sanFrancisco") {
+      $("#franciscoResults").show();
+    } else if (userSelection === "joshua") {
+      $("#joshuaResults").show();
+    };
+
+    const name = LocationBook1.findLocation(1).name;
+    console.log("name = " + name);
+    // const landmark =
+
+    // const time =
+    // const notes =
+
+    // let newLocation = new Location (name, landmark, time, notes);
+
+    // const name = $(".resultName").val();
+    // const landmark = $(".landMarks").val();
+
+    // const time = $(".timeOfYear").val();
+    // const notes = $(".notes").val();
+
+    // let newLocation = new Location (name, landmark, time, notes);
+
+      
+  });
+});
+
+
+// const flavor = $("input:radio[name=flavor]:checked").val();
+/*
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+  });
+
+four cards with the location title on top
+event listener: when title card is clicked, corresponding results div shows
+what we will need:
+  - event listener
+  - conditional statement, which shows one item depending on which item is clicked.
+
+
+
+
+Constructors:
+  * Each of us
+    1. Ella
+    2. Anna
+  * Locations
+  * Keys in Locations:
+    - Landmarks - array
+    - time of year - string
+    - notes - string
+
+
+UI Logic:
+
+
+*/
